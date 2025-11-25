30db9c73d35f4eb9bcccc52af84c8bedcb290e1c
Daria Dmitrienko <ishmizh@gmail.com>
2025-11-25 15:30:48 +0400

    верстка блока с формой

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -3,32 +3,42 @@ import { useAppDispatch, useAppSelector } from "../../services/store";
 import styles from "./Cart.module.scss";
 import { deleteProduct } from "../../services/slices/OrderSlice"
 import type { IProduct } from "../data/ALL_PRODUCTS";
+import { useState } from "react";
 
 
 
 export default function Cart() {
   const cart = useAppSelector(getOrder);
   const dispatch = useAppDispatch();
+  const [telegram, setTelegram] = useState("");
 
   function handleDelete(product: IProduct) {
     dispatch(deleteProduct(product));
   };
 
   return (<>
-    <h1>Cart</h1>
-    <div className={styles.cart}>
-      {cart.map(product => <div className={styles.productAtCart}>
-        <img src={product.images[0]} />
-        <div className={styles.productName}>{product.name}</div>
-        <div className={styles.productPrice}> {product.price} GEL</div>
-        <button onClick={() => handleDelete(product)} className={styles.deleteButton}>✖️</button>
-      </div>
-      )}
-      <div>
-        <label className={styles.label}>Ваш никнейм в телеграме для связи</label>
-        <input type="text" className={styles.inputField} />
-      </div>
 
+    <div className={styles.cartContainer}>
+
+      <div className={styles.cart}>
+        <h1>Cart</h1>
+        {cart.map(product => <div className={styles.productAtCart}>
+          <img src={product.images[0]} />
+          <div className={styles.productName}>{product.name}</div>
+          <div className={styles.productPrice}> {product.price} GEL</div>
+          <button onClick={() => handleDelete(product)} className={styles.deleteButton}>✖️</button>
+        </div>
+        )}
+
+        <form method="post">
+          <p className={styles.label}>Your nickname in Telegram for communication</p>
+          <input type="text" className={styles.inputField} value={telegram}
+            onChange={(e) => setTelegram(e.target.value)} />
+          <button className={styles.orderButton}>Order</button>
+        </form>
+        <p className={styles.label}>After ordering through the cart. I will contact you for payment and further processing. Or you can place an order through the Instagram by contacting me personally.
+          For orders to another country the shipping cost is calculated individually.</p>
+      </div>
     </div>
   </>)
 
