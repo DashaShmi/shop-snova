ed685a53bf82bcb2ea7ab688c92565c771cec3bc
Daria Dmitrienko <ishmizh@gmail.com>
2025-11-28 16:12:46 +0400

    счетчик суммы товаров в корзине

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -12,6 +12,15 @@ export default function Cart() {
   const dispatch = useAppDispatch();
   const [telegram, setTelegram] = useState("");
 
+  const totalPrice = cart.reduce((sum, product) => {
+    return sum + product.price;
+  }, 0);
+
+  const sendTelegram = (e: React.FormEvent) => {
+    e.preventDefault();                // не даем странице перезагружаться
+    console.log("Никнейм:", telegram);
+  };
+
   function handleDelete(product: IProduct) {
     dispatch(deleteProduct(product));
   };
@@ -27,22 +36,23 @@ export default function Cart() {
           <div className={styles.productName}>{product.name}</div>
           <div className={styles.productPrice}> {product.price} GEL</div>
           <button onClick={() => handleDelete(product)} className={styles.deleteButton}>✖️</button>
+
         </div>
         )}
 
-        <form method="post">
+        <form onSubmit={sendTelegram} method="post">
+          <div className={styles.line} />
+          <div className={styles.total}>Estimated Total: {totalPrice} GEL</div>
           <p className={styles.label}>Your nickname in Telegram for communication</p>
           <input type="text" className={styles.inputField} value={telegram}
             onChange={(e) => setTelegram(e.target.value)} />
-          <button className={styles.orderButton}>Order</button>
+          <button type="submit" className={styles.orderButton}>Order</button>
         </form>
         <p className={styles.label}>After ordering through the cart. I will contact you for payment and further processing. Or you can place an order through the Instagram by contacting me personally.
           For orders to another country the shipping cost is calculated individually.</p>
       </div>
     </div>
   </>)
-
-
 
 }
 
