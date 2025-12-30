ccf6497dd7fc2b426eefab75ba8b8cbc4c2463e1
Daria Dmitrienko <ishmizh@gmail.com>
2025-12-30 20:46:14 +0400

    backend test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,5 @@
+import { ALL_PRODUCTS_BACKEND } from "./ALL_PRODUCTS_BACKEND";
+
 console.log('Ты вонючка?')
 
 const brevoApiKey = process.env.BREVO_API_KEY ?? "";
@@ -9,39 +11,61 @@ if (brevoApiKey.length === 0) {
 
 console.log("brevo api key: " + brevoApiKey)
 
-async function sendEmail() {
-  const response = await fetch("https://api.brevo.com/v3/smtp/email", {
-    method: "POST",
-    headers: {
-      "accept": "application/json",
-      "content-type": "application/json",
-      "api-key": brevoApiKey
-    },
-    body: JSON.stringify({
-      sender: {
-        name: "Dasha ne Brevno",
-        email: "ishmizh@gmail.com",
-      },
-      to: [
-        {
-          email: "ishmizh@gmail.com",
-          name: "Fonte",
-        },
-      ],
-      subject: "Hello from Brevno!",
-      htmlContent: `
-      <html>
-        <body>
-          <p>Hello,</p>
-          <p>This is my first transactional email sent from Brevo.</p>
-        </body>
-      </html>
-    `,
-    }),
+// const response = await fetch("https://api.brevo.com/v3/smtp/email", {
+//   method: "POST",
+//   headers: {
+//     "accept": "application/json",
+//     "content-type": "application/json",
+//     "api-key": brevoApiKey
+//   },
+//   body: JSON.stringify({
+//     sender: {
+//       name: "Dasha ne Brevno",
+//       email: "ishmizh@gmail.com",
+//     },
+//     to: [
+//       {
+//         email: "ishmizh@gmail.com",
+//         name: "Fonte",
+//       },
+//     ],
+//     subject: "Hello from Brevno!",
+//     htmlContent: `
+//     <html>
+//       <body>
+//         <p>Hello,</p>
+//         <p>This is my first transactional email sent from Brevo.</p>
+//       </body>
+//     </html>
+//   `,
+//   }),
+// });
+
+// const data = await response.json();
+// console.log(data);
+
+
+
+async function sendEmail(ids: string[]) {
+
+  let mail_text = "";
+
+  mail_text += "Даша, привет!\n"
+  mail_text += "Новый заказ на сайте!\n"
+
+  const orderedProducts = ids
+    .map(id => ALL_PRODUCTS_BACKEND.find(p => p.id === id))
+    .filter(Boolean); // убираем undefined
+
+  orderedProducts.forEach(product => {
+    mail_text += `* ${product!.name}\n`;
   });
 
-  const data = await response.json();
-  console.log(data);
+
+  mail_text += "* ИМЯ_ТОВАРА \n"
+
+
+  console.log(mail_text);
 }
 
-sendEmail();
+sendEmail(["bifold-wallet", "zippered-wallet"]);
